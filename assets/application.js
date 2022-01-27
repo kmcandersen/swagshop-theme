df0e2629c6a1817b50b6302543338c6f6a0f2522
kmcandersen <kristen.andersen@gmail.com>
2022-01-27 12:43:40 -0600

    add mini-cart

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,45 @@
-// Put your application javascript here
+const store = Vue.reactive({
+    state: {
+        cartState: []
+    },
+
+    getCart() {
+        axios.get('/cart.js')
+            .then(res => {
+                // replacing old cart w/new on refresh
+                this.state.cartState.unshift(res.data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+})
+
+const miniCartState = Vue.reactive({
+    hidden: true
+})
+
+const toggleMiniCart = {
+    openCart(){
+        miniCartState.hidden = !miniCartState.hidden;
+        menuState.hidden = true;
+    }
+}
+
+const menuState = Vue.reactive({
+    hidden: true
+})
+
+const toggleMenu = {
+    openMenu() {
+        menuState.hidden = !menuState.hidden;
+        miniCartState.hidden = true;
+    }
+}
+
+const toggleClose = {
+    closeAll() {
+        miniCartState.hidden = true;
+        menuState.hidden = true;
+    }
+}
